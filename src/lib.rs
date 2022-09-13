f40ce3235c3e97bd82bd9b98b9ad6bc739e3f8d6
Vortex Acherontic <vortex@z-ray.de>
2022-09-13 17:34:10 +0200

    Init lib and some fn skeletons

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,48 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub enum ZyypCallback {
+    ZyppPackagesNotFound(Vec<String>),
+    ZyppSuccess,
+    ZyppTimeOut,
+    ZyppRepoExists,
+    ZyppNotImplemented
+}
+
+pub fn install() -> ZyypCallback {
+    return ZyypCallback::ZyppNotImplemented;
+}
+
+pub fn uninstall(clean_deps:bool) -> ZyypCallback {
+    return ZyypCallback::ZyppNotImplemented;
+}
+
+pub fn refresh(force_refresh:bool) -> ZyypCallback {
+    return ZyypCallback::ZyppNotImplemented;
+}
+
+pub fn info(package:&str) -> String{
+    return "NotImplemented".to_string();
+}
+
+pub fn add_lock(packages:Vec<String>) -> ZyypCallback{
+    return ZyypCallback::ZyppNotImplemented;
+}
+
+pub fn remove_lock(packages:Vec<String>) -> ZyypCallback {
+    return ZyypCallback::ZyppNotImplemented;
+}
+
+pub fn add_repo(url:&str, name:&str, priority:i16) -> ZyypCallback {
+    return ZyypCallback::ZyppNotImplemented;
+}
+
+pub fn remove_repo(name:&str) -> ZyypCallback {
+    return ZyypCallback::ZyppNotImplemented;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        
     }
 }
