d728660284fd4ac72640164662414c5ea17c133b
Vortex Acherontic <vortex@z-ray.de>
2022-09-13 19:38:06 +0200

    Repo related fn to dedicated mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod repo;
 pub enum ZyypCallback {
     ZyppPackagesNotFound(Vec<String>),
     ZyppSuccess,
@@ -14,10 +15,6 @@ pub fn uninstall(clean_deps:bool) -> ZyypCallback {
     return ZyypCallback::ZyppNotImplemented;
 }
 
-pub fn refresh(force_refresh:bool) -> ZyypCallback {
-    return ZyypCallback::ZyppNotImplemented;
-}
-
 pub fn info(package:&str) -> String{
     return "NotImplemented".to_string();
 }
@@ -27,14 +24,6 @@ pub fn add_lock(packages:Vec<String>) -> ZyypCallback{
 }
 
 pub fn remove_lock(packages:Vec<String>) -> ZyypCallback {
-    return ZyypCallback::ZyppNotImplemented;
-}
-
-pub fn add_repo(url:&str, name:&str, priority:i16) -> ZyypCallback {
-    return ZyypCallback::ZyppNotImplemented;
-}
-
-pub fn remove_repo(name:&str) -> ZyypCallback {
     return ZyypCallback::ZyppNotImplemented;
 }
 
---
+++ src/repo.rs
@@ -0,0 +1,13 @@
+use crate::ZyypCallback;
+
+pub fn refresh(sudo_tool:&str, force_refresh:bool) -> ZyypCallback {
+    return ZyypCallback::ZyppNotImplemented;
+}
+
+pub fn add_repo(sudo_tool:&str, url:&str, name:&str, priority:i16) -> ZyypCallback {
+    return ZyypCallback::ZyppNotImplemented;
+}
+
+pub fn remove_repo(sudo_tool:&str, name:&str) -> ZyypCallback {
+    return ZyypCallback::ZyppNotImplemented;
+}
