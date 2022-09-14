9c308b3785de17f46ca54f10127518f99766fec0
Vortex Acherontic <vortex@z-ray.de>
2022-09-14 20:06:55 +0200

    Add zypp binding ground work and tests

--- src/repo.rs
+++ src/repo.rs
@@ -1,7 +1,7 @@
 pub mod repo {
     #[link(name = "zypp")]
     extern {
-        fn _createRepo(name_r:&str);
+        
     }
 
     use crate::ZyppCallback;
@@ -10,11 +10,25 @@ pub mod repo {
         return ZyppCallback::ZyppNotImplemented;
     }
     
-    pub fn add_repo(sudo_tool:&str, url:&str, name:&str, priority:i16) -> ZyppCallback {
+    pub fn add_repo(sudo_tool:&str, url:&str, name:&str, priority:i32) -> ZyppCallback {
+        let x = unsafe {
+            
+        };
         return ZyppCallback::ZyppNotImplemented;
     }
     
     pub fn remove_repo(sudo_tool:&str, name:&str) -> ZyppCallback {
         return ZyppCallback::ZyppNotImplemented;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ZyppCallback;
+    use super::repo::add_repo;
+    
+    #[test]
+    fn it_works() {
+        assert!(matches!(add_repo("sudo", "url", "name", 90), ZyppCallback::ZyppNotImplemented));
     }
 }
